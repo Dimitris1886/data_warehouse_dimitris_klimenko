6e24c5b672aaf13bb178646e35c964372d192934
Dimitris1886 <dimaklimenko90@gmail.com>
2024-08-26 18:41:21 +0200

    Roles, dlt Setup,uv venv

---
+++ setup_DLT/06_setup_dlt/.dlt/check_data.sql
@@ -0,0 +1,18 @@
+USE WAREHOUSE dev_wh;
+SHOW ROLES;
+
+USE ROLE movies_reader;
+USE DATABASE movies;
+
+SHOW SCHEMAS;
+
+USE SCHEMA staging;
+
+SHOW TABLES;
+
+SELECT * FROM netflix;
+
+DESC TABLE netflix;
+
+SELECT COUNT(*) FROM netflix;
+
